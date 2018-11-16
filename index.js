87418b0ccc4074105d839546f5c4febf1e9ca24c
talexcrowell <talexcrowell@gmail.com>
2018-11-16 08:24:35 -0800

    reworked endpoint

--- index.js
+++ index.js
@@ -22,8 +22,26 @@ app.use(
   })
 );
 
-app.get('/api/cheeses', (res, req, next) =>{
-  
+app.get('/api/cheeses', (req, res) =>{
+  res.json([
+    'Bath Blue',
+    'Barkham Blue',
+    'Buxton Blue',
+    'Cheshire Blue',
+    'Devon Blue',
+    'Dorset Blue Vinney',
+    'Dovedale',
+    'Exmoor Blue',
+    'Harbourne Blue',
+    'Lanark Blue',
+    'Lymeswold',
+    'Oxford Blue',
+    'Shropshire Blue',
+    'Stichelton',
+    'Stilton',
+    'Blue Wensleydale',
+    'Yorkshire Blue'
+  ]);
 });
 
 function runServer(port = PORT) {
